79ca8564c196e555a20ab4f60e20dc77a61335cb
KlaitonRicken <klaitonricken@hotmail.com>
2019-04-24 19:12:47 -0300

    aula1

---
+++ frontQuarta/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Klaitons';
+
+  categorias = [
+    {id:1, nome:"Categoria 1"},
+    {id:1, nome:"Categoria 2"},
+    {id:1, nome:"Categoria 3"},
+    {id:1, nome:"Categoria 4"},
+    {id:1, nome:"Categoria 5"},
+    {id:1, nome:"Categoria 6"},
+    {id:1, nome:"Categoria 7"},
+    {id:1, nome:"Categoria 8"},
+    {id:1, nome:"Categoria 9"},
+    {id:1, nome:"Categoria 10"},
+  ];
+}
